655a1e65c3efe7ca9c42e5a47961061908adaff9
yongjunlee <93720125+2hapu@users.noreply.github.com>
2023-10-30 22:09:03 +0900

    feat: 사용하지 않는 import문 제거

--- src/main/java/com/happiday/Happi_Day/domain/service/EventCommentService.java
+++ src/main/java/com/happiday/Happi_Day/domain/service/EventCommentService.java
@@ -13,10 +13,8 @@ import com.happiday.Happi_Day.exception.CustomException;
 import com.happiday.Happi_Day.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
--- src/main/java/com/happiday/Happi_Day/domain/service/EventService.java
+++ src/main/java/com/happiday/Happi_Day/domain/service/EventService.java
@@ -17,11 +17,9 @@ import com.happiday.Happi_Day.exception.ErrorCode;
 import com.happiday.Happi_Day.utils.FileUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
