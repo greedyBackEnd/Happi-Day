e5b2d5e022362e6df759058b595003b98f30998b
kdybe01 <kdybe01@gmail.com>
2023-11-13 18:56:08 +0900

    feat : 팀 구독 및 취소 로직 추가

--- src/main/java/com/happiday/Happi_Day/domain/repository/TeamRepository.java
+++ src/main/java/com/happiday/Happi_Day/domain/repository/TeamRepository.java
@@ -1,10 +1,17 @@
 package com.happiday.Happi_Day.domain.repository;
 
 import com.happiday.Happi_Day.domain.entity.team.Team;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
     Optional<Team> findByName(String teamName);
+
+    @Query("SELECT t FROM Team t WHERE t.id NOT IN (SELECT ut.id FROM User u JOIN u.subscribedTeams ut WHERE u.id = :userId)")
+    Page<Team> findUnsubscribedTeams(@Param("userId") Long userId, Pageable pageable);
 }
