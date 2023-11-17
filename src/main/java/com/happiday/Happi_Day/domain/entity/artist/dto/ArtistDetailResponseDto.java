56ee2be2aac2683c0f259d7dcab50a0a051e7284
kdybe01 <kdybe01@gmail.com>
2023-11-17 17:23:57 +0900

    feat : 아티스트 조회 시, 이벤트/상품함께 조회

--- src/main/java/com/happiday/Happi_Day/domain/entity/artist/dto/ArtistDetailResponseDto.java
+++ src/main/java/com/happiday/Happi_Day/domain/entity/artist/dto/ArtistDetailResponseDto.java
@@ -1,6 +1,8 @@
 package com.happiday.Happi_Day.domain.entity.artist.dto;
 
 import com.happiday.Happi_Day.domain.entity.artist.Artist;
+import com.happiday.Happi_Day.domain.entity.event.dto.EventListResponseDto;
+import com.happiday.Happi_Day.domain.entity.product.dto.SalesListResponseDto;
 import com.happiday.Happi_Day.domain.entity.team.dto.TeamListResponseDto;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +18,8 @@ public class ArtistDetailResponseDto {
     private String profileUrl;
     private boolean isSubscribed;
     private List<TeamListResponseDto> teams;
+    private List<SalesListResponseDto> sales;
+    private List<EventListResponseDto> events;
 
     public static ArtistDetailResponseDto of(Artist artist, boolean isSubscribed) {
         return ArtistDetailResponseDto.builder()
@@ -35,6 +39,24 @@ public class ArtistDetailResponseDto {
                 .profileUrl(artist.getProfileUrl())
                 .isSubscribed(isSubscribed)
                 .teams(teams)
+                .build();
+    }
+
+    public static ArtistDetailResponseDto of(
+            Artist artist,
+            boolean isSubscribed,
+            List<TeamListResponseDto> teams,
+            List<SalesListResponseDto> sales,
+            List<EventListResponseDto> events) {
+        return ArtistDetailResponseDto.builder()
+                .id(artist.getId())
+                .name(artist.getName())
+                .description(artist.getDescription())
+                .profileUrl(artist.getProfileUrl())
+                .isSubscribed(isSubscribed)
+                .teams(teams)
+                .sales(sales)
+                .events(events)
                 .build();
     }
 }
