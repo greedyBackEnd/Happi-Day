4f543e79c2fbb360bffbef0cdde8b8f1b37208bb
kdybe01 <kdybe01@gmail.com>
2024-01-25 23:14:20 +0900

    fix : 카테고리 이름, 유저 이름으로 반환하도록 수정

--- src/main/java/com/happiday/Happi_Day/domain/entity/product/dto/SalesListResponseDto.java
+++ src/main/java/com/happiday/Happi_Day/domain/entity/product/dto/SalesListResponseDto.java
@@ -1,9 +1,7 @@
 package com.happiday.Happi_Day.domain.entity.product.dto;
 
 import com.happiday.Happi_Day.domain.entity.product.Sales;
-import com.happiday.Happi_Day.domain.entity.product.SalesCategory;
 import com.happiday.Happi_Day.domain.entity.product.SalesStatus;
-import com.happiday.Happi_Day.domain.entity.user.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,16 +9,16 @@ import lombok.Getter;
 @Builder
 public class SalesListResponseDto {
     private Long id;
-    private SalesCategory salesCategory;
-    private User user;
+    private String categoryName;
+    private String userNickName;
     private String name;
     private SalesStatus salesStatus;
 
     public static SalesListResponseDto of(Sales sales) {
         return SalesListResponseDto.builder()
                 .id(sales.getId())
-                .salesCategory(sales.getSalesCategory())
-                .user(sales.getUsers())
+                .categoryName(sales.getSalesCategory().getName())
+                .userNickName(sales.getUsers().getNickname())
                 .name(sales.getName())
                 .salesStatus(sales.getSalesStatus())
                 .build();
