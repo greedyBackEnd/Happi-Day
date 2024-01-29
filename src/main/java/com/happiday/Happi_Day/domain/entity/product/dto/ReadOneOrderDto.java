e624ca04008bc6aec28b93e8f9ac37219fbe6b48
csumin0825 <csumin008255@gmail.com>
2024-01-29 16:41:25 +0900

    refactor: 주문 상세 내역 response 수정

--- src/main/java/com/happiday/Happi_Day/domain/entity/product/dto/ReadOneOrderDto.java
+++ src/main/java/com/happiday/Happi_Day/domain/entity/product/dto/ReadOneOrderDto.java
@@ -13,39 +13,50 @@ import java.util.Map;
 @Getter
 public class ReadOneOrderDto {
     private Long id;
-    private String username;
     private Long salesId;
-    private Map<String, Integer> orderedProducts;
-    private Integer price;
-    private OrderStatus orderStatus;
+    private String salesName;
+    private String sellerAccount;
+    private String username;
+    private String userPhone;
     private String address;
-    private String orderAt;
-    private String delivery;
-    private String trackingNum;
     private String depositor;
     private String refundAccount;
-    private String sellerAccount;
+    private String orderAt;
+    private Map<String, Integer> orderedProducts;
+    private Map<String, Integer> productPrice;
+    private Map<String, Integer> delivery;
+    private Integer totalPrice;
+    private OrderStatus orderStatus;
+    private String trackingNum;
+
 
     public static ReadOneOrderDto fromEntity(Sales sales, Order order, User user){
         Map<String, Integer> productList = new HashMap<>();
+        Map<String, Integer> priceList = new HashMap<>();
         for (OrderedProduct orderedProduct: order.getOrderedProducts()) {
             productList.put(orderedProduct.getProduct().getName(),orderedProduct.getQuantity());
+            priceList.put(orderedProduct.getProduct().getName(), orderedProduct.getProduct().getPrice()*orderedProduct.getQuantity());
         }
+        Map<String, Integer> delivery = new HashMap<>();
+        delivery.put(order.getDelivery().getName(), order.getDelivery().getPrice());
 
         return ReadOneOrderDto.builder()
                 .id(order.getId())
-                .username(user.getNickname())
                 .salesId(sales.getId())
-                .orderAt(order.getOrderedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
-                .price(order.getTotalPrice())
-                .orderedProducts(productList)
-                .orderStatus(order.getOrderStatus())
+                .salesName(sales.getName())
+                .sellerAccount(sales.getAccount())
+                .username(user.getNickname())
+                .userPhone(user.getPhone())
                 .address(order.getAddress())
-                .delivery(order.getDelivery().getName())
-                .trackingNum(order.getTrackingNum() != null ? order.getTrackingNum(): "등록되지 않음.")
                 .depositor(order.getDepositor())
                 .refundAccount(order.getRefundAccount())
-                .sellerAccount(sales.getAccount())
+                .orderAt(order.getOrderedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
+                .orderedProducts(productList)
+                .productPrice(priceList)
+                .delivery(delivery)
+                .totalPrice(order.getTotalPrice())
+                .orderStatus(order.getOrderStatus())
+                .trackingNum(order.getTrackingNum() != null ? order.getTrackingNum(): "등록되지 않음.")
                 .build();
     }
 }
