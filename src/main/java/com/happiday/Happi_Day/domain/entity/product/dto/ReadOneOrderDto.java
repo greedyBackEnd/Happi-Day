0594ad50a102dab6f017e5d2450fe44644020c5f
csumin0825 <csumin008255@gmail.com>
2024-01-30 17:26:15 +0900

    refactor: 주문 상세 내역 response 수정

--- src/main/java/com/happiday/Happi_Day/domain/entity/product/dto/ReadOneOrderDto.java
+++ src/main/java/com/happiday/Happi_Day/domain/entity/product/dto/ReadOneOrderDto.java
@@ -6,7 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Builder
@@ -22,23 +23,65 @@ public class ReadOneOrderDto {
     private String depositor;
     private String refundAccount;
     private String orderAt;
-    private Map<String, Integer> orderedProducts;
-    private Map<String, Integer> productPrice;
-    private Map<String, Integer> delivery;
+    private List<OrderedProductDto> orderedProducts;
+    private Integer productPrice;
+    private DeliveryDto delivery;
     private Integer totalPrice;
     private OrderStatus orderStatus;
     private String trackingNum;
 
+    @Builder
+    @Getter
+    private static class OrderedProductDto {
+        private String productName;
+        private int count;
+        private int individualProductPrice;
 
-    public static ReadOneOrderDto fromEntity(Sales sales, Order order, User user){
-        Map<String, Integer> productList = new HashMap<>();
-        Map<String, Integer> priceList = new HashMap<>();
-        for (OrderedProduct orderedProduct: order.getOrderedProducts()) {
-            productList.put(orderedProduct.getProduct().getName(),orderedProduct.getQuantity());
-            priceList.put(orderedProduct.getProduct().getName(), orderedProduct.getProduct().getPrice()*orderedProduct.getQuantity());
+        private OrderedProductDto(String productName, int count, int individualProductPrice){
+            this.productName = productName;
+            this.count = count;
+            this.individualProductPrice = individualProductPrice;
         }
-        Map<String, Integer> delivery = new HashMap<>();
-        delivery.put(order.getDelivery().getName(), order.getDelivery().getPrice());
+    }
+
+    @Builder
+    @Getter
+    private static class DeliveryDto{
+        private String deliveryWay;
+        private int price;
+
+        private DeliveryDto(String deliveryWay, int price){
+            this.deliveryWay = deliveryWay;
+            this.price = price;
+        }
+    }
+
+
+    public static ReadOneOrderDto fromEntity(Sales sales, Order order, User user) {
+//        Map<String, Integer> productList = new HashMap<>();
+//        Map<String, Integer> priceList = new HashMap<>();
+//        for (OrderedProduct orderedProduct : order.getOrderedProducts()) {
+//            productList.put(orderedProduct.getProduct().getName(), orderedProduct.getQuantity());
+//            priceList.put(orderedProduct.getProduct().getName(), orderedProduct.getProduct().getPrice() * orderedProduct.getQuantity());
+//        }
+//        Map<String, Integer> delivery = new HashMap<>();
+//        delivery.put(order.getDelivery().getName(), order.getDelivery().getPrice());
+
+        List<OrderedProductDto> productList = new ArrayList<>();
+        for(OrderedProduct orderedProduct : order.getOrderedProducts()){
+//            productList.add(new Product(orderedProduct.getProduct().getName(),orderedProduct.getQuantity(), orderedProduct.getProduct().getPrice()));
+            OrderedProductDto orderedProductDto = OrderedProductDto.builder()
+                    .productName(orderedProduct.getProduct().getName())
+                    .count(orderedProduct.getQuantity())
+                    .individualProductPrice(orderedProduct.getProduct().getPrice())
+                    .build();
+            productList.add(orderedProductDto);
+        }
+
+        DeliveryDto deliveryDto = DeliveryDto.builder()
+                .deliveryWay(order.getDelivery().getName())
+                .price(order.getDelivery().getPrice())
+                .build();
 
         return ReadOneOrderDto.builder()
                 .id(order.getId())
@@ -52,11 +95,11 @@ public class ReadOneOrderDto {
                 .refundAccount(order.getRefundAccount())
                 .orderAt(order.getOrderedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
                 .orderedProducts(productList)
-                .productPrice(priceList)
-                .delivery(delivery)
+                .productPrice(order.getTotalPrice()-order.getDelivery().getPrice())
+                .delivery(deliveryDto)
                 .totalPrice(order.getTotalPrice())
                 .orderStatus(order.getOrderStatus())
-                .trackingNum(order.getTrackingNum() != null ? order.getTrackingNum(): "등록되지 않음.")
+                .trackingNum(order.getTrackingNum() != null ? order.getTrackingNum() : "등록되지 않음.")
                 .build();
     }
 }
